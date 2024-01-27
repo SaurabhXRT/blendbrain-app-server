1e4fc3479b93daab0af100d57f55657edc9975b5
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-27 12:03:27 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const SecretKey = "2809a95eedde5863d8e8e3bea5205cd62d290b10a3769afee677b8754a4d05b7";
 
 router.post("/login", async (req, res) => {
-    const { mobileNumber } = req.body;
+    const  mobileNumber  = req.body.mobileNumber;
 
     try {
         const existinguser = await User.findOne({ mobileNumber });
