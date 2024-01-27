b6dd17b2a7ee26e0ef8abcdfada2222f041b3d9a
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-27 11:54:06 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,15 +4,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    
   },
   mobileNumber: {
     type: String,
-    required: true,
+  
   },
   password: {
     type: String,
-    required: true,
+ 
   },
   isVerified: {
     type: Boolean,
