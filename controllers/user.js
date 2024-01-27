019330d370ed4e228234d1fe2b19575088e5d49a
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-27 17:18:00 +0530

    Create user.js

---
+++ controllers/user.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/auth');
+const cloudinary = require("cloudinary").v2;
+const User = require('../models/user');
+const Post = require('../models/userpost');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+cloudinary.config({
+  cloud_name: "dar4ws6v6",
+  api_key: "131471632671278",
+  api_secret: "d0UW2ogmMnEEMcNVcDpzG33HKkY",
+});
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb('invalid image file!', false);
+  }
+};
+const uploads = multer({ storage, fileFilter });
+
+
+
+router.use(authMiddleware);
+
+router.get('/fetch-user', async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json({
+      mobileNumber: user.mobile,
+      profileImage: user.profileImage,
+    });
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
