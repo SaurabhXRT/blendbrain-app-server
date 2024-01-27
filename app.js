f4b6b211af66fe5897bc44b15ab1730ad55d5a69
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-27 10:09:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+const db = async () => {
+    const uri =
+        "mongodb+srv://saurabhkumar:rVKACHYbuzYy7VMs@cluster0.n4zogin.mongodb.net/brainbulbreactnative?retryWrites=true&w=majority";
+    try {
+        mongoose.set("strictQuery", false);
+        mongoose.connect(uri);
+        const db = mongoose.connection;
+        db.on("error", console.error.bind(console, "connection error:"));
+        db.once("open", () => {
+            console.log("Connected to MongoDB...");
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+db();
+
+const authRoutes = require('./controllers/auth');
+app.use('/auth', authRoutes);
+const port = 3000;
+app.listen(port, () => {
+    console.log(`app is running at port number ${port}`);
+});
---
+++ controllers/auth.js
@@ -0,0 +1,36 @@
+// controllers/auth.js
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const app = express();
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: true }));
+const SecretKey = "2809a95eedde5863d8e8e3bea5205cd62d290b10a3769afee677b8754a4d05b7";
+
+router.post("/login", async (req, res) => {
+    const { mobileNumber } = req.body;
+
+    try {
+        const existinguser = await User.findOne({ mobileNumber });
+        if (!existinguser) {
+            const user = new User(mobileNumber);
+            await user.save();
+            const token = jwt.sign({ id: user._id }, SecretKey, { expiresIn: '7d' });
+            res.json({ token });
+        } else {
+            existinguser.mobileNumber = mobileNumber;
+            await  existinguser.save();
+            const token = jwt.sign({ id:  existinguser._id }, SecretKey, { expiresIn: '7d' });
+            res.json({ token });
+        }
+    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;
+
