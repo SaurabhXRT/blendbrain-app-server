3149160591d97c3b694f63ec781ec53fd0b69d44
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-29 00:56:00 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -259,4 +259,41 @@ router.post("/connect/:userId", async (req, res) => {
   }
 });
 
+router.put('/updateprofile',uploads.single('profileImage'), async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    console.log(user);
+    user.name = req.body.name || user.name;
+    user.username = req.body.username || user.username;
+    user.year = req.body.year || user.year;
+    user.branch = req.body.branch || user.branch;
+    user.college = req.body.college || user.college;
+    //user.studyingIn = req.body.studyingIn || user.studyingIn;
+    user.bio = req.body.bio || user.bio;
+    //user.profileImage = req.body.profileImage || user.profileImage;
+    let profileimage;
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path);
+      profileimage = result.secure_url;
+    }
+     //  //console.log(req.file);
+     //  const result = await cloudinary.uploader.upload(req.file.path);
+     //  console.log("result");
+     //  user.profileImage = result.secure_url || user.profileImage;
+     // // fs.unlinkSync(req.file.path);
+    user.profileImage = profileimage || user.profileImage;
+    
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    console.error('Error updating user profile:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 module.exports = router;
