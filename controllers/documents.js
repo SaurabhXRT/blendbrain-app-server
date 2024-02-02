69dd8fe368ed3f40463ea7b14ab042d7b9a63349
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-02-03 00:42:32 +0530

    Update documents.js

--- controllers/documents.js
+++ controllers/documents.js
@@ -24,11 +24,11 @@ const fileFilter = (req, file, cb) => {
 };
 const upload = multer({ storage, fileFilter });
 
-const googleDriveKeyPath = process.env.GOOGLE_DRIVE_KEY_PATH;
+//const googleDriveKeyPath = process.env.GOOGLE_DRIVE_KEY_PATH;
 const { google } = require('googleapis');
 const drive = google.drive('v3');
 const auth = new google.auth.GoogleAuth({
-  keyFile: googleDriveKeyPath,
+  keyFile: "/key.json",
   scopes: ['https://www.googleapis.com/auth/drive'],
 });
 
