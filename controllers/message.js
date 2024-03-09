19759d6e7fbdf47f79fec4e9605e2efeefecb6ca
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-03-09 11:03:23 +0530

    Update message.js

--- controllers/message.js
+++ controllers/message.js
@@ -1,35 +1,33 @@
-// app.js
-const express = require("express");
-const mongoose = require("mongoose");
-const http = require("http");
+// controllers/message.js
 const socketIo = require("socket.io");
-const Message = require("../models/Message");
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const Message = require("../models/message"); // Assuming you have a Message model already defined
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+let io;
 
-  socket.on("sendMessage", async ({ sender, receiver, text }) => {
-    try {
-      const message = new Message({
-        sender: sender,
-        receiver: receiver,
-        content: text
-      });
-      await message.save();
-      io.to(receiver).emit("message", message);
-    } catch (error) {
-      console.error("Error saving message:", error);
-    }
+// Initialize WebSocket server
+const initSocket = (server) => {
+  io = socketIo(server);
+  io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("sendMessage", async ({ sender, receiver, text }) => {
+      try {
+        const message = new Message({
+          sender: sender,
+          receiver: receiver,
+          content: text
+        });
+        await message.save();
+        io.to(receiver).emit("message", message);
+      } catch (error) {
+        console.error("Error saving message:", error);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
   });
+};
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+module.exports = { initSocket };
